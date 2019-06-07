f57a0b0f7e0506ed7f467e322db4f114c050cc4d
giantoppai <37357748+giantoppai@users.noreply.github.com>
2019-06-07 16:23:12 +0000

    Remove duplicate bookmark

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,12 +1,15 @@
+var bookmarkId;
+
 function startup() {
-	console.log("Initializing add-on...");
+    console.log("Initializing add-on...");
 }
 
 // show page action when bookmark is created on current tab
-browser.bookmarks.onCreated.addListener(function showPageAction() {
+browser.bookmarks.onCreated.addListener(function showPageAction(id) {
+  bookmarkId = id; // save bookmark id
   var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
   gettingActiveTab.then((tabs) => {
-	console.log("showing page action");
+    console.log("showing page action");
     browser.pageAction.show(tabs[0].id);
   });
 });
@@ -15,9 +18,19 @@ browser.bookmarks.onCreated.addListener(function showPageAction() {
 browser.bookmarks.onRemoved.addListener(function hidePageAction() {
   var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
   gettingActiveTab.then((tabs) => {
-	console.log("hiding page action");
+    console.log("hiding page action");
     browser.pageAction.hide(tabs[0].id);
   });
 });
 
+// handle messages
+function handleMessage(request) {
+  if (request.greeting = "Adding Bookmark To Folder") {
+    var removingBookmark = browser.bookmarks.remove(bookmarkId);
+  }
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
+
 startup();
+
