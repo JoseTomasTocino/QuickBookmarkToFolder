b05993a30d88c7886bf92d950e4c8bb7c4198209
giantoppai <37357748+giantoppai@users.noreply.github.com>
2019-06-07 14:21:15 +0000

    Create background.js

---
+++ app/scripts/background.js
@@ -0,0 +1,23 @@
+function startup() {
+	console.log("Initializing add-on...");
+}
+
+// show page action when bookmark is created on current tab
+browser.bookmarks.onCreated.addListener(function showPageAction() {
+  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+  gettingActiveTab.then((tabs) => {
+	console.log("showing page action");
+    browser.pageAction.show(tabs[0].id);
+  });
+});
+
+// hide page action when bookmark is removed on current tab
+browser.bookmarks.onRemoved.addListener(function hidePageAction() {
+  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+  gettingActiveTab.then((tabs) => {
+	console.log("hiding page action");
+    browser.pageAction.hide(tabs[0].id);
+  });
+});
+
+startup();
