97b2c5b85e0783ebaa13ef6d87c4716963093168
José Tomás Tocino <theom3ga@gmail.com>
2020-10-02 21:50:28 +0200

    Avoid duplicate entries in recent folders

--- src/popup/storageHandler.js
+++ src/popup/storageHandler.js
@@ -1,4 +1,4 @@
-import * as util from "./util";
+import * as util from "../util";
 
 const MAX_RECENT_FOLDERS = 10;
 
@@ -16,6 +16,11 @@ export default class StorageHandler
             } else {
                 recentFolders = [];
             }
+
+            // // Make the folders unique
+            // recentFolders = [...new Set(recentFolders)];
+
+            console.log(recentFolders);
 
             if (recentFolders.length > 0) {
                 callback(recentFolders);
@@ -38,6 +43,10 @@ export default class StorageHandler
                 while (recentFolders.length > MAX_RECENT_FOLDERS) {
                     recentFolders.shift();
                 }
+
+                // Check if folder is not already saved
+                if(recentFolders.filter(e => e.id === folderId).length > 0)
+                    return;
 
                 recentFolders.push({id: folderId, title: folderName});
 
